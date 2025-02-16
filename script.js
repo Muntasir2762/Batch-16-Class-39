7bc4175575bb24d8aee99e64744797541e5fa996
Munotasir2762 <muntasirpranto2762@gmail.com>
2025-02-16 23:30:50 +0600

    Js function

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function sub(number1, number2)
+{
+    let digit1 = number1;
+    let digit2 = number2;
+
+    let result = digit1-digit2;
+
+    console.log("The sub is="+result);
+}
+
+function sum(x, y)
+{
+    let digit1 = x;
+    let digit2 = y;
+
+    let result = digit1+digit2;
+
+    console.log("The sum is="+result);
+}
+
+sub(50, 30);
+
+sub(90, 30);
+
+sum(67, 90);
